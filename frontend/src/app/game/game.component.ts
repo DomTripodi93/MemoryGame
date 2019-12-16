e9a1187a809c8414681b382369809293244aa6cd
dominic <dtripodi93@gmail.com>
2019-12-16 00:51:11 -0500

    set up selection of game size through drop-down

--- frontend/src/app/game/game.component.ts
+++ frontend/src/app/game/game.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { GameService } from './game.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-game',
@@ -7,17 +8,24 @@ import { GameService } from './game.service';
   styleUrls: ['./game.component.css']
 })
 export class GameComponent implements OnInit {
+  @ViewChild('gameSizeSelect', {static: false}) gameSizeForm: NgForm;
   cards = []
   base: number;
+  gameSize = [12, 20, 40];
+  defaultSize = 12;
 
   constructor(
     private game: GameService
   ) { }
 
   ngOnInit() {
-    this.cards = this.game.setCards(10);
+  }
+
+  setGameSize(){
+    this.base = this.gameSizeForm.value.size/2;
+    this.cards = this.game.setCards(this.base);
     this.base = this.cards.length;
     this.cards = this.game.shuffleCards(this.cards);
-    console.log(this.cards);
   }
+
 }
